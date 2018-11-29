53a6c1a630d106a18659efa0dd8cc5f417a00386
Ricardo <shirotokuro3715@gmail.com>
2018-11-29 17:46:00 -0400

    Update to state in amcharts map

--- src/components/Maps-amchats.js
+++ src/components/Maps-amchats.js
@@ -5,43 +5,107 @@ import * as am4maps from '@amcharts/amcharts4/maps';
 import am4geodata_rd from '@amcharts/amcharts4-geodata/dominicanRepublicHigh';
 import am4themes_dark from '@amcharts/amcharts4/themes/dark';
 import am4themes_animated from '@amcharts/amcharts4/themes/animated';
-
+import data from './data.json';
+import _ from 'lodash';
 export default class MapsAmchats extends Component {
-  componentDidMount() {
-    am4core.useTheme(am4themes_dark);
-    am4core.useTheme(am4themes_animated);
+	constructor(props) {
+		super();
+		this.state = {
+			provinceName    : '',
+			provinceID      : '',
+			provinceWeather : '',
+			provinceSoil    : '',
+			provinceAgrop   : ''
+		};
+		this.renderProvince = this.renderProvince.bind(this);
+		this.mapData = this.mapData.bind(this);
+	}
+	componentDidMount() {
+		am4core.useTheme(am4themes_dark);
+		am4core.useTheme(am4themes_animated);
 
-    let map = am4core.create('chartdiv', am4maps.MapChart);
-    map.geodata = am4geodata_rd;
-    map.projection = new am4maps.projections.Miller();
+		let map = am4core.create('chartdiv', am4maps.MapChart);
+		map.geodata = am4geodata_rd;
+		map.projection = new am4maps.projections.Miller();
 
-    let polygonSeries = new am4maps.MapPolygonSeries();
-    polygonSeries.useGeodata = true;
-    map.series.push(polygonSeries);
+		let polygonSeries = new am4maps.MapPolygonSeries();
+		polygonSeries.useGeodata = true;
+		map.series.push(polygonSeries);
 
-    // Configure series
-    let polygonTemplate = polygonSeries.mapPolygons.template;
-    polygonTemplate.tooltipText = '{name}';
-    polygonTemplate.fill = am4core.color('#757575');
+		polygonSeries.data = data.provincias;
+		/**
+     *  Todo's (display only and static data): 
+     * Agregar form para registrar usuario
+     * Agregar registro de parcelas (registradas en su municipio)
+     * El usuario debe ser capaz de registrar parcelas y los tipos de suelo
+     * Desplegar datos de division administrativa del terreno por parcelas
+     */
 
-    // Create hover state and set alternative fill color
-    let hs = polygonTemplate.states.create('hover');
-    hs.properties.fill = am4core.color('#3c5bdc');
+		/* polygonSeries.data = [
+      { id: 'DO-32', name: 'Santo Domingo', agrp: 612.4, mine: 0 }
+    ]; */
 
-    map.exporting.menu = new am4core.ExportMenu();
-    this.map = map;
-  }
-  componentWillUnmount() {
-    if (this.chart) {
-      this.chart.dispose();
-    }
-  }
+		// Configure series
+		let polygonTemplate = polygonSeries.mapPolygons.template;
+		// polygonTemplate.tooltipText = '{name} Agrp: {soil.use.agrp} Temperatura: {weather.main.temp}';
+		polygonTemplate.tooltipText = '{name} Agrp: {soil.use.agrp} Temperatura: {agrop_use.production.rice}';
+		polygonTemplate.fill = am4core.color('#757575');
+		polygonTemplate.events.on(
+			'hit',
+			(ev) =>
+				this.setState((state) => {
+					return {
+						...state,
+						dataProvince    : ev.target._dataItem._dataContext,
+						provinceName    : ev.target._dataItem._dataContext.name,
+						provinceID      : ev.target._dataItem._dataContext.id,
+						provinceWeather : ev.target._dataItem._dataContext.weather,
+						provinceSoil    : ev.target._dataItem._dataContext.soil,
+						provinceAgrop   : ev.target._dataItem._dataContext.agrop_use
+					};
+				}),
+			this
+		);
 
-  render() {
-    return (
-      <Container>
-        <div id='chartdiv' style={{ width: '100%', height: '500px' }} />
-      </Container>
-    );
-  }
+		// Create hover state and set alternative fill color
+		let hs = polygonTemplate.states.create('hover');
+		hs.properties.fill = am4core.color('#3c5bdc');
+
+		map.exporting.menu = new am4core.ExportMenu();
+		this.map = map;
+		console.log(data);
+	}
+	componentWillUnmount() {
+		if (this.chart) {
+			this.chart.dispose();
+		}
+	}
+
+	renderProvince(dataProvince) {
+		return (
+			<div>
+				<h1>
+					<span>
+						<strong>{this.state.provinceID} </strong>
+					</span>{' '}
+					{this.state.provinceName}
+				</h1>
+				<h3>Produccion</h3>
+				{this.mapData(this.state.dataProvince)}
+			</div>
+		);
+	}
+
+	mapData(provinceData) {
+		console.log(this.state);
+	}
+
+	render() {
+		return (
+			<Container>
+				<div id='chartdiv' style={{ width: '100%', height: '500px' }} />
+				{this.renderProvince(this.state)}
+			</Container>
+		);
+	}
 }
