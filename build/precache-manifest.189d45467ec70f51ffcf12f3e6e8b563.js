c1299ae2b9d22b66a18478aaa703fc456bdc709e
Ricardo Bibieca <shirotokuro3715@gmail.com>
2018-12-08 17:01:33 -0400

    gitignore fix

---
+++ build/precache-manifest.189d45467ec70f51ffcf12f3e6e8b563.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fbbaccbdbcc939aa8a9",
+    "url": "/static/js/3.5fbbaccb.chunk.js"
+  },
+  {
+    "revision": "4ef7c688ddd29cab7a64",
+    "url": "/static/css/main.ad47b756.chunk.css"
+  },
+  {
+    "revision": "29f4143f1a513b7f1e0f",
+    "url": "/static/js/xlsx.29f4143f.chunk.js"
+  },
+  {
+    "revision": "6a13c1222a6c1b3c581b",
+    "url": "/static/js/runtime~main.6a13c122.js"
+  },
+  {
+    "revision": "487694e1453b8f6684a6",
+    "url": "/static/js/2.487694e1.chunk.js"
+  },
+  {
+    "revision": "4ef7c688ddd29cab7a64",
+    "url": "/static/js/main.4ef7c688.chunk.js"
+  },
+  {
+    "revision": "7e15e41181683d7e8e66",
+    "url": "/static/js/4.7e15e411.chunk.js"
+  },
+  {
+    "revision": "27092055a1639a0c0d42",
+    "url": "/static/js/5.27092055.chunk.js"
+  },
+  {
+    "revision": "baab73c618a8e2dcfc03",
+    "url": "/static/js/6.baab73c6.chunk.js"
+  },
+  {
+    "revision": "487694e1453b8f6684a6",
+    "url": "/static/css/2.c96c458f.chunk.css"
+  },
+  {
+    "revision": "5f7ac86aa20311b56ab07b087d8b9f58",
+    "url": "/index.html"
+  }
+];
