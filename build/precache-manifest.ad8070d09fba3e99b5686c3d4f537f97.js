f2b3656c7f835fef481ac1c113766d14fea82e75
Ricardo Bibieca <42103135+Xhoria-RB@users.noreply.github.com>
2019-02-15 09:33:26 -0400

    Add server to build production (#4)
    Merged

---
+++ build/precache-manifest.ad8070d09fba3e99b5686c3d4f537f97.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "0863e598b90600049921",
+    "url": "/static/css/main.f0661a3d.chunk.css"
+  },
+  {
+    "revision": "0863e598b90600049921",
+    "url": "/static/js/main.0863e598.chunk.js"
+  },
+  {
+    "revision": "29f4143f1a513b7f1e0f",
+    "url": "/static/js/xlsx.29f4143f.chunk.js"
+  },
+  {
+    "revision": "32c5b1f392038f8149d7",
+    "url": "/static/css/2.2593c9a6.chunk.css"
+  },
+  {
+    "revision": "32c5b1f392038f8149d7",
+    "url": "/static/js/2.32c5b1f3.chunk.js"
+  },
+  {
+    "revision": "5fbbaccbdbcc939aa8a9",
+    "url": "/static/js/3.5fbbaccb.chunk.js"
+  },
+  {
+    "revision": "1c0f92c51b57b2e94940",
+    "url": "/static/js/4.1c0f92c5.chunk.js"
+  },
+  {
+    "revision": "27092055a1639a0c0d42",
+    "url": "/static/js/5.27092055.chunk.js"
+  },
+  {
+    "revision": "baab73c618a8e2dcfc03",
+    "url": "/static/js/6.baab73c6.chunk.js"
+  },
+  {
+    "revision": "6dafc08cfad38394ad74",
+    "url": "/static/js/runtime~main.6dafc08c.js"
+  },
+  {
+    "revision": "2a05d8f90773a751bb74d6ac95403c60",
+    "url": "/index.html"
+  }
+];
